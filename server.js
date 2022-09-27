ed9ec6a2523f6edfe51ad3533c519ad4c6507d06
Charlie Snow <cesnow@wpi.edu>
2022-09-27 19:01:49 -0400

    Added some stuff

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require( 'http' ),
+      fs   = require( 'fs' ),
+      mime = require( 'mime' ),
+      dir  = 'public/',
+      port = 3000
+
+const handleGet = function( request, response ) {
+  const filename = dir + request.url.slice( 1 ) 
+
+  if( request.url === '/' ) {
+    sendFile( response, 'public/index.html' )
+  }else{
+    sendFile( response, filename )
+  }
+}
+
+const sendFile = function( response, filename ) {
+   const type = mime.getType( filename ) 
+
+   fs.readFile( filename, function( err, content ) {
+
+     // if the error = null, then we've loaded the file successfully
+     if( err === null ) {
+
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { 'Content-Type': type })
+       response.end( content )
+
+     }else{
+
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( '404 Error: File Not Found' )
+
+     }
+   })
+}
+
+server.listen( process.env.PORT || port )
